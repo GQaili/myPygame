fc6406765a8244d5ca988d39131fefe3a19181a0
GQaili <gezim.qaili@gmail.com>
2026-05-13 20:55:25 +0200

    new changes 2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
+
 import pygame
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 
 def main():
@@ -35,6 +37,12 @@ def main():
                 return
         
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
         for sprite in drawable:
             sprite.draw(screen)
